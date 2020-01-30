864514b4053f264f288064c998a8b79e406c2562
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2020-01-30 18:38:15 +0800

    more initial stuff

--- gopcaml.ml
+++ gopcaml.ml
@@ -1,7 +1,19 @@
-open! Core_kernel
-open! Ecaml
+open Core
+open Ecaml
 
 let version = 0.1
+
+let print_region start_pos end_pos : unit =
+  let print_region_internal () = 
+    let text = Current_buffer.contents
+      ~start:start_pos
+      ~end_:end_pos
+      () in
+    Text.to_utf8_bytes text
+    |>  message
+  in
+  ignore (print_region_internal ())
+      
 
 let () =
   defun
@@ -12,9 +24,23 @@ let () =
     |}
     (Returns Value.Type.int)
     (let open Defun.Let_syntax in
-     
      return 1
      );
+  defun
+    ("gopcaml-print-region" |> Symbol.intern)
+    [%here]
+    ~docstring:{|
+    Prints the text between START and END.
+    |}
+    (Returns Value.Type.unit)
+    (let open Defun.Let_syntax in
+     let%map_open startm = required "start" Value.Type.value
+     and endm  = Defun.required "end" Value.Type.value in
+     let startm = Position.of_value_exn startm in
+     let endm = Position.of_value_exn endm in
+     print_region startm endm
+    );
+
   defun
     ("gopcaml-test" |> Symbol.intern)
     [%here]
--- gopcaml_mode.el
+++ gopcaml_mode.el
@@ -1,7 +1,19 @@
+(defun fake-module-reload (module)
+  (let ((tmpfile (make-temp-file
+                  (file-name-nondirectory module) nil module-file-suffix)))
+    (copy-file module tmpfile t)
+    (module-load tmpfile)))
 
 (add-to-list 'load-path (expand-file-name "./_build/default/"))
 (require 'gopcaml)
+(fake-module-reload "./_build/default/gopcaml.so")
+(gopcaml-print-region)
+(gopcaml-print-region)
 (gopcaml-version)
 
+
+(defun gopcaml-mode-print-region (start end)
+  (interactive "r")
+  (gopcaml-print-region start end))
 
 (provide 'gopcaml-mode)
